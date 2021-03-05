67692bac41ad50c55c0ce71f133c0fa5ebea0584
Natália Salvino <natalia.andre@ccc.ufcg.edu.br>
2021-03-05 09:05:23 -0300

    Connecting with api

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./styles.css";
+
+export default function Dashboard() {
+    
+    return (
+        <div className="container">
+           
+            
+        </div>
+    );
+}
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import icon from '../../assets/chatbot.svg';
+import api from '../../services/api';
 
 import "./styles.css";
 
 export default function Login() {
-  return (
-    <div className="container">
-        <div className="content">
+    const [pin, setPin] = useState('');
 
-            <img src={ icon } alt="logo"></img>
-            <h2>Casperbot Painel de Controle</h2>
-            <input placeholder="digite seu pin"></input>
-            <button type="submit">login</button>
-            </div>
+    async function handleSubmit(event) {
+        event.preventDefault();
+        console.log(pin)
+        const response = await api.get('/user', {headers: { pin }});
         
+        console.log(response);
+    }
+
+    return (
+        <div className="container">
+            <div className="content">
+                <img src={ icon } alt="logo"></img>
+                <h2>Casperbot Painel de Controle</h2>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        type="password" 
+                        placeholder="digite seu pin" 
+                        value={pin} 
+                        onChange={event => setPin(event.target.value) }>
+                    </input>
+                    <button type="submit">login</button>
+                </form>
+                
+            </div>
+            
         </div>
-  );
+    );
 }
